da90b26ae8961603e297e0e33e12f80245a311fd
Heavy <Heavy4@mail.ru>
2022-12-07 13:50:28 +0300

    3213123

---
+++ task25/task25.cs
@@ -0,0 +1,11 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+int ok = a;
+for (int i = 1; i < b; i++)
+{
+ok = ok * a;
+}
+Console.WriteLine("A в степени B равно: " + ok);
---
+++ task29/task29.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Random array = new Random();
+int[]n = new int[8];
+for (int i = 0; i < n.Length; i++)
+{
+n[i] = new Random().Next(-10,10);
+Console.Write(n[i] +",");
+}
+//line for commit 
